7617fa1b596ba68c5b9c7feff5c4c3335202e774
VanshGolakiya-2910 <vanshgolakiya11@gmail.com>
2025-01-19 21:01:46 +0530

    taskmanger change

--- backend/routes/taskroutes.js
+++ backend/routes/taskroutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { getAllTasks } = require('../Controllers/taskController');
+const { getAllTasks, addTask } = require('../Controllers/taskController');
 const router = express.Router();
 
 router.get('/tasks', getAllTasks);
+router.post('/tasks', addTask);  // Add this line to handle POST requests
 
 module.exports = router;
