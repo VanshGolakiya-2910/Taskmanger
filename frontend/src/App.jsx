285d589b025c3d59550f8de4127a31325714f670
VanshGolakiya-2910 <vanshgolakiya11@gmail.com>
2025-12-30 23:09:02 +0530

    added : frontend with tailwind dependencies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-50 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-slate-900">
+        Tailwind v4 Verified
+      </h1>
+    </div>
+  );
+}
